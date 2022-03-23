e72e292744e1dee6f58907acb94d402229e7d34d
Tomohiro Omata <37297654+vowyy@users.noreply.github.com>
2022-03-23 10:23:37 +0900

    var let constの違い

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import "./styles.css";
+var val1 = "val1";
+console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var変数は上書き可能
+val1 = "違う値";
+console.log(val1);
+
+// var変数は再宣言可能
+var val1 = "再宣言";
+console.log(val1);
+
+let val2 = "let変数";
+console.log(val2);
+
+// letは上書き可能
+val2 = "上書きval2";
+console.log(val2);
+
+// let変数は再宣言不可
+let val2 = "再宣言";
+console.log(val2);
+
+const val3 = "const変数";
+console.log(val3);
+
+// constは上書き不可
+val3 = "上書きval2";
+console.log(val3);
+
+// const変数は再宣言不可
+let val3 = "再宣言";
+console.log(val3);
