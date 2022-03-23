d2fbf81608f4bee7ada7b86d8a55ef8a141bdaef
Tomohiro Omata <37297654+vowyy@users.noreply.github.com>
2022-03-23 10:51:27 +0900

    デォフルト値

--- src/index.js
+++ src/index.js
@@ -86,10 +86,14 @@ console.log(func1("小俣です"));
 // const message2 = `名前は${name}です。年齢は${age}歳です。`
 // console.log(message2);
 
-const myProfile = ["友広", 23];
-const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
-console.log(message3);
+// const myProfile = ["友広", 23];
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
 
-const [name, age] = myProfile;
-const message4 = `名前は${name}です。年齢は${age}歳です。`;
-console.log(message4);
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+//デォフルト値
+const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん`);
+console.log(sayHello("小俣"));
