22b0a9320ef3f124ec6a498aab507259497ffdd4
Tomohiro Omata <37297654+vowyy@users.noreply.github.com>
2022-03-23 15:19:30 +0900

    スプレッド構文

--- src/index.js
+++ src/index.js
@@ -94,6 +94,44 @@ console.log(func1("小俣です"));
 // const message4 = `名前は${name}です。年齢は${age}歳です。`;
 // console.log(message4);
 
-//デォフルト値
-const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん`);
-console.log(sayHello("小俣"));
+// //デォフルト値
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん`);
+// console.log(sayHello("小俣"));
+
+// スプレッド構文
+//配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1)
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+//まとめる
+// const arr2 = [1,2,3,4,5]
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// //配列のコピー
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+// const arr6 = [...arr4];
+
+// console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5 ];
+// console.log(arr7);
+
+// const ar8 = arr4
+
+// // で配列コピーして変更するとコピーもとも変更してしまう
+
+//  const arr8 = arr5
+//  console.log(arr5);
+//  arr8[0] = 800
+
+//  console.log(arr5)
+//  console.log(arr8)
